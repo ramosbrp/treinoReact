42b15530d2eec618a9aac1d8d9323e050e54024b
Bruno <bruno.rp45@gmail.com>
2021-08-11 17:29:45 -0300

    Treino de useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 
 function App(props) {
-  // const [ usuario, setUsuario ] = useState('Bruno');
+  const [ contagem, setContagem ] = useState( 0 );
+
+  const aumentar = () => {
+    setContagem( contagem + 1 );
+  };
 
   return (
     <>
       {/* <h1>{ props.title } { props.user } </h1> */}
-      <p> { usuario } </p>
+      {/* <p> { usuario } </p> */}
+      <p> { props.soma } </p>
       <input placeholder="usuário" className="usuarioInput" name="usuario" id="usuario" ></input>
       <button type="button" >Pesquisar</button>
+      <div> { contagem } </div>
+      <button onClick= { aumentar } >Clique para aumentar</button>
     </>
   );
 }
